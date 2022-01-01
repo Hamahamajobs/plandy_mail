5f70eb0b3bb9ea318434005b7c1ec05dd2438c25
Kazutaka Hamada <hama0102105@gmail.com>
2022-01-01 13:47:07 +0900

    mail

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 
 // POSTされたJSON文字列を取り出し
 $body = file_get_contents("php://input");
-echo $body;
-exit();
+
 if (is_null($body)) {
     # error データが無い
     http_response_code(500);        //HTTPレスポンスコード(500サーバーエラー)
@@ -12,6 +11,7 @@ if (is_null($body)) {
 }
 
 $json = json_decode($body, true);   //JSONに変換　第二引数をTrueにすると連想配列になる
+
 if (is_null($json)) {
     # error JSONをデコードできない
     http_response_code(500);        //HTTPレスポンスコード(500サーバーエラー)
@@ -19,9 +19,12 @@ if (is_null($json)) {
     exit();
 }
 
+
+$email_to = $params['email_to']; // メール送信先 
+
 http_response_code(200);
 header("Content-Type: application/json; charset=utf-8");
-echo json_encode($json);
+echo $email_to;
 
 // JSON文字列をobjectに変換
 //   ⇒ 第2引数をtrueにしないとハマるので注意
@@ -32,29 +35,6 @@ echo json_encode($json);
 // $subject = "TEST";
 // $message = "This is TEST.\r\nHow are you?";
 // $headers = "From: hama1050102@gmail.com";
-
-// $array = [
-//     'tokyo' => [
-//         '品川',
-//         '五反田',
-//         '三軒茶屋',
-//         '四谷'
-//     ],
-//     'kanagawa' => [
-//         '横浜',
-//         '相模原',
-//         '湘南',
-//         '鎌倉'
-//     ],
-//     'saitama' => [
-//         '所沢',
-//         '狭山',
-//         '川口',
-//         '浦和',
-//         '小手指',
-//         '飯能'
-//     ]
-// ];
 
 // print(mail($to, $subject, $message, $headers));
 // header("Content-Type: application/json; charset=utf-8");
