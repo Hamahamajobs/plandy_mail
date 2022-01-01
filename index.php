35e21b809bfa59f14fc9a7318297145a6bf19e48
Kazutaka Hamada <hama0102105@gmail.com>
2022-01-01 12:37:02 +0900

    mail4

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $to = "jobs1050102@gmail.com";
 $subject = "TEST";
 $message = "This is TEST.\r\nHow are you?";
-$headers = "From: hama105dddddd0102@gmail.com";
+$headers = "From: hama1050102@gmail.com";
 
 print(mail($to, $subject, $message, $headers));
 
