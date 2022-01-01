cac670fd948fd3064c1ba99ad13d20c4dded28b8
Kazutaka Hamada <hama0102105@gmail.com>
2022-01-01 13:37:01 +0900

    mail

--- index.php
+++ index.php
@@ -1,17 +1,35 @@
 <?php
 
 // POSTされたJSON文字列を取り出し
-$json = file_get_contents("php://input");
+$body = file_get_contents("php://input");
+if (is_null($body)) {
+    # error データが無い
+    http_response_code(500);        //HTTPレスポンスコード(500サーバーエラー)
+    echo "no data (JSON)";
+    exit();
+}
+
+$json = json_decode($body, true);   //JSONに変換　第二引数をTrueにすると連想配列になる
+if (is_null($json)) {
+    # error JSONをデコードできない
+    http_response_code(500);        //HTTPレスポンスコード(500サーバーエラー)
+    echo "JSON error";
+    exit();
+}
+
+http_response_code(200);
+header("Content-Type: application/json; charset=utf-8");
+echo json_encode($json);
 
 // JSON文字列をobjectに変換
 //   ⇒ 第2引数をtrueにしないとハマるので注意
-$contents = json_decode($json, true);
+// $contents = json_decode($body, true);
 
 
-$to = "jobs1050102@gmail.com";
-$subject = "TEST";
-$message = "This is TEST.\r\nHow are you?";
-$headers = "From: hama1050102@gmail.com";
+// $to = "jobs1050102@gmail.com";
+// $subject = "TEST";
+// $message = "This is TEST.\r\nHow are you?";
+// $headers = "From: hama1050102@gmail.com";
 
 // $array = [
 //     'tokyo' => [
@@ -37,6 +55,6 @@ $headers = "From: hama1050102@gmail.com";
 // ];
 
 // print(mail($to, $subject, $message, $headers));
-header("Content-Type: application/json; charset=utf-8");
-echo json_encode($contents);
+// header("Content-Type: application/json; charset=utf-8");
+// echo json_encode($contents);
 
